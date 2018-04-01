b473c89303ae635caf1ef00deafc4701b12c71f6
Heth213 <35584988+Heth213@users.noreply.github.com>
2018-04-01 13:36:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,169 @@
+//Initial Vars
+//const bdoBossAlert = require(__dirname+'/bdoBossAlert.js');
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+//var emitter = require('./emitter');
+var bossTime = require('./bdoBossTimes')
+var moment = require('moment');
+
+/////////////////////////////////////////
+////Discord Bot
+////////////////////////////////////////
+//Run On Bot Login
+bot.on('ready', () => {
+
+	console.log('Online...');
+});
+
+
+	  var bossHrs = {
+	kutum: {kzHrs: " "},
+    dastardBheg: {bhHrs: " "},
+    dimTreeSpirit: {dtHrs: " "},
+    giantMudster: {mdHrs: " "},
+    karanda: {kaHrs: " "},
+    kzarka: {kzHrs: " "},
+    redNose: {rnHrs: " "},
+    nouver: {nvHrs: " "}
+	  }
+
+	//bot timer stuff
+	function sendBossTimers() {
+		if(!bossTime.kzarkaT && !bossTime.bhegT && !bossTime.dtT && !bossTime.mudT && !bossTime.rnT){
+			console.log("Not all times ready.");
+
+		}else{
+
+
+	kzTrimed = bossTime.kzarkaN.trim();
+	bhTrimed = bossTime.bhegN.trim();
+	dtTrimed = bossTime.dtN.trim();
+	mdTrimed = bossTime.mudN.trim();
+	rnTrimed = bossTime.rnN.trim();
+
+	kzSliced = kzTrimed.slice(22,-4);
+	bhSliced = bhTrimed.slice(22,-4);
+	dtSliced = dtTrimed.slice(22,-4);
+	mdSliced = mdTrimed.slice(22,-4);
+	rnSliced = rnTrimed.slice(22,-4);
+
+	kzSliced2 = kzTrimed.slice(0,-21);
+	bhSliced2 = bhTrimed.slice(0,-21);
+	dtSliced2 = dtTrimed.slice(0,-21);
+	mdSliced2 = mdTrimed.slice(0,-21);
+	rnSliced2 = rnTrimed.slice(0,-21);
+
+	//get all times and silce the shit out of them
+	let kzTime = bossTime.kzarkaT.slice(3,-6);
+	let bhTime = bossTime.bhegT.slice(3,-6);
+	let dtTime = bossTime.dtT.slice(3,-6);
+	let mdTime = bossTime.mudT.slice(3,-6);
+	let rnTime = bossTime.rnT.slice(3,-6);
+
+
+
+
+	//render numbers/days in english
+	moment.locale('en');
+	var serverTime = moment().add(3, 'hours').format("ddd, HH:mm");
+	//console.log('serverTime: '+ serverTime);
+	let now = moment(serverTime,'ddd, HH:mm');
+	let kz = moment(kzTime,'ddd, HH:mm');
+	let bh = moment(bhTime,'ddd, HH:mm');
+	let dt = moment(dtTime,'ddd, HH:mm');
+	let md = moment(mdTime,'ddd, HH:mm');
+	let rn = moment(rnTime,'ddd, HH:mm');
+	 // console.log('now :' + now);
+	 // 	console.log('kz :' + kz);
+	 // 		console.log('bh :' + bh);
+	 // 			console.log('dt :' + dt);
+	 // 				console.log('md :' + md);
+	 // 					console.log('rn :' + rn);
+
+
+	 //making it render in arabic
+	 moment.locale('ar');
+	 //checks everything
+	 var kzAfter = moment.duration(kz - now).humanize();
+	 var bhAfter = moment.duration(bh - now).humanize();
+	 var dtAfter = moment.duration(dt - now).humanize();
+	 var mdAfter = moment.duration(md - now).humanize();
+	 var rnAfter = moment.duration(rn - now).humanize();
+	 if (kz > now) {
+	 	var kzDone = '[-](بعد ' + kzAfter + ')\n <البداية: '+kzSliced2+'>\n\n';}
+	 	else {kzDone = '[سيظهر قريبا](-)[الحد الاقصى: '+kzSliced+']\n\n';}
+
+	 	if (bh > now) {
+	 		var bhDone = '[-](بعد ' + bhAfter+ ')\n <البداية: '+bhSliced2+'>\n\n';}
+	 		else {bhDone = '[سيظهر قريبا](-)[الحد الاقصى: '+bhSliced+']\n\n';}
+
+	 		if (dt > now) {
+	 			var dtDone = '[-](بعد ' + dtAfter+ ')\n <البداية: '+dtSliced2+'>\n\n';}
+	 			else {dtDone = '[سيظهر قريبا](-)[الحد الاقصى: '+dtSliced+']\n\n';}
+
+	 			if (md > now) {
+	 				var mdDone = '[-](بعد ' + mdAfter+ ')\n <البداية: '+mdSliced2+'>\n\n';}
+	 				else {mdDone = '[سيظهر قريبا](-)[الحد الاقصى: '+mdSliced+']\n\n';}
+
+	 				if (rn > now) {
+	 					var rnDone = '[-](بعد ' + rnAfter+ ')\n <البداية: '+rnSliced2+'>\n\n';}
+	 					else {rnDone = '[سيظهر قريبا](-)[الحد الاقصى: '+rnSliced+']\n\n';}
+
+
+
+	// SEND TO CHANNEL
+	//set channel id
+
+	if (bossHrs.kzarka.kzHrs != null && bossHrs.dastardBheg.bhHrs != null && bossHrs.dimTreeSpirit.dtHrs != null && bossHrs.giantMudster.mdHrs != null && bossHrs.redNose.rnHrs != null){
+	if (bossHrs.kzarka.kzHrs !== kzAfter || bossHrs.dastardBheg.bhHrs !== bhAfter || bossHrs.dimTreeSpirit.dtHrs !== dtAfter || bossHrs.giantMudster.mdHrs !== mdAfter || bossHrs.redNose.rnHrs !== rnAfter){
+	var bossChannel = bot.channels.get('429950207250137088');  //#timers [Lunarium]
+	//deleting & sending embed 
+	bossChannel.fetchMessages({ limit: 1})
+	.then(messages => {
+		const fetchedMsg = messages.first(); 
+		fetchedMsg.delete();
+	});
+	bossChannel.send(" ** توقعات ظهور الزعماء ** ```md\n# كزاركا - Kzarka  \n <المتوقع: "+kzTime+" >                             "+kzDone+" \n\n# بهق - Bheg  \n <المتوقع: "+bhTime+" >                             "+bhDone+" \n\n# ابو خشم احمر - Red Nose  \n <المتوقع: "+rnTime+" >                             "+rnDone+" \n\n# الشجرة - Dim Tree Spirit  \n <المتوقع: "+dtTime+" >                             "+dtDone+" \n\n# ابو الوحل - Giant Mudster  \n <المتوقع: "+mdTime+" >                             "+mdDone+" ``` \n\n** التحديث مباشر **");
+	  bossHrs.kzarka.kzHrs = moment.duration(kz - now).humanize();
+	  bossHrs.dastardBheg.bhHrs = moment.duration(bh - now).humanize();
+	  bossHrs.dimTreeSpirit.dtHrs = moment.duration(dt - now).humanize();
+	  bossHrs.giantMudster.mdHrs = moment.duration(md - now).humanize();
+	  bossHrs.redNose.rnHrs = moment.duration(rn - now).humanize();
+	}else{}
+}else{
+	console.log('all null');
+}
+};
+
+
+
+
+
+
+setTimeout(sendBossTimers, 15000);
+	}//end of func
+	sendBossTimers();
+
+
+
+   //setting the bot's game name :)
+   function settheGame(){
+   	if(!bossTime.kzarkaT){
+   		console.log("kzarka time is undefined");
+
+   	}else{
+
+   		let kzTime = bossTime.kzarkaT.slice(8,-6);
+
+   		bot.user.setGame("توقع كزاركا: " + kzTime.trim());
+
+
+
+   	};
+   	setTimeout(settheGame, 60000);
+   };
+   settheGame();
+
+
+//Login Bot User
+bot.login(process.env.B0T_ToKEN);
---
+++ bdoBossTimes.js
@@ -0,0 +1,63 @@
+//Initial Variables
+var cheerio = require("cheerio");
+var request  = require("request");
+
+var kzarkaNext = "";
+var kzarkaEst = "";
+var bhegNext = "";
+var bhegEst = "";
+function retrieveTimeData(){
+            //Url for time spawn data
+			var url = "http://urzasarchives.com/bdo/wbtbdo/wbtmena/";
+            
+            
+					request(url, function (error, response, body) {
+			    	var $ = cheerio.load(body);
+			    
+			    	try{
+
+                        kzarkaNext = $("table").next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
+						kzarkaEst = $("table").next().next().next().next().next().find("tbody > tr").next().next().next().find("td").next().html();
+						
+						bhegNext = $("table").next().find("tbody > tr").next().next().find("td").next().html();
+						bhegEst = $("table").next().find("tbody > tr").next().next().next().find("td").next().html();
+						
+						dtNext = $("table").next().next().find("tbody > tr").next().next().find("td").next().html();
+						dtEst = $("table").next().next().find("tbody > tr").next().next().next().find("td").next().html();	
+
+						mudNext = $("table").next().next().next().find("tbody > tr").next().next().find("td").next().html();
+						mudEst = $("table").next().next().next().find("tbody > tr").next().next().next().find("td").next().html();	
+
+						rnNext = $("table").next().next().next().next().next().next().find("tbody > tr").next().next().find("td").next().html();
+						rnEst = $("table").next().next().next().next().next().next().find("tbody > tr").next().next().next().find("td").next().html();
+
+							module.exports.kzarkaN = kzarkaNext;
+							module.exports.kzarkaT = kzarkaEst;
+				
+							module.exports.bhegN = bhegNext;
+							module.exports.bhegT = bhegEst;
+
+							module.exports.dtN = dtNext;
+							module.exports.dtT = dtEst;
+
+							module.exports.mudN = mudNext;
+							module.exports.mudT = mudEst;
+
+							module.exports.rnN = rnNext;
+							module.exports.rnT = rnEst;
+			    	}
+					catch(htmlError){
+                        console.log(htmlError+"\nCould not find HTML element!"+"\n Retrying Now!... ");
+					}
+			});
+};
+
+
+
+
+function endlessLoop(){
+    retrieveTimeData();
+    setTimeout(endlessLoop, 15000);
+}
+endlessLoop();
+retrieveTimeData();
